db6d30c0d1fe152e4f0139069b5397e5da76a446
magodo <wztwcy@gmail.com>
2024-12-04 14:29:12 +1100

    schema-generator: Simplify the azurerm one

--- .tools/schema-generator/azurerm/main.go
+++ .tools/schema-generator/azurerm/main.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/provider"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	tfschema "github.com/magodo/tfadd/schema"
 	"github.com/magodo/tfpluginschema"
 )
@@ -20,48 +18,15 @@ func main() {
 }
 
 func run() error {
-	resources := map[string]*schema.Resource{}
-	for _, service := range provider.SupportedTypedServices() {
-		for _, rs := range service.Resources() {
-			wrapper := sdk.NewResourceWrapper(rs)
-			rsWrapper, err := wrapper.Resource()
-			if err != nil {
-				return fmt.Errorf("wrapping Resource %q: %+v", rs.ResourceType(), err)
-			}
-			resources[rs.ResourceType()] = rsWrapper
-		}
-	}
-	for _, service := range provider.SupportedUntypedServices() {
-		for name, rs := range service.SupportedResources() {
-			resources[name] = rs
-		}
-	}
+	provider := provider.AzureProvider()
 	rschs := map[string]*tfschema.Schema{}
-	for name, rs := range resources {
+	for name, rs := range provider.ResourcesMap {
 		rschs[name] = &tfschema.Schema{Block: tfpluginschema.FromSDKv2SchemaMap(rs.Schema)}
 	}
-
-	datasources := map[string]*schema.Resource{}
-	for _, service := range provider.SupportedTypedServices() {
-		for _, ds := range service.DataSources() {
-			wrapper := sdk.NewDataSourceWrapper(ds)
-			dsWrapper, err := wrapper.DataSource()
-			if err != nil {
-				return fmt.Errorf("wrapping DataSource %q: %+v", ds.ResourceType(), err)
-			}
-			datasources[ds.ResourceType()] = dsWrapper
-		}
-	}
-	for _, service := range provider.SupportedUntypedServices() {
-		for name, ds := range service.SupportedDataSources() {
-			datasources[name] = ds
-		}
-	}
 	dschs := map[string]*tfschema.Schema{}
-	for name, ds := range datasources {
+	for name, ds := range provider.DataSourcesMap {
 		dschs[name] = &tfschema.Schema{Block: tfpluginschema.FromSDKv2SchemaMap(ds.Schema)}
 	}
-
 	b, err := json.MarshalIndent(tfschema.ProviderSchema{ResourceSchemas: rschs, DatasourceSchemas: dschs}, "", "  ")
 	if err != nil {
 		return err
